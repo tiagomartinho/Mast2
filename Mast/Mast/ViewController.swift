e5fd8126dc4bd5af996e6d3de019490cfbf80df1
Shihab Mehboob <shihab@IHQs-MacBook-Pro.local>
2019-09-11 16:18:51 +0100

    Initial tab bar

--- Mast/Mast/ViewController.swift
+++ Mast/Mast/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UITabBarController, UITabBarControllerDelegate {
     func createTabBar() {
         DispatchQueue.main.async {
             let symbolConfig = UIImage.SymbolConfiguration(pointSize: 22, weight: .regular)
+            
             let im1 = UIImage(systemName: "bubble.middle.bottom", withConfiguration: symbolConfig)?.withTintColor(UIColor.black.withAlphaComponent(1), renderingMode: .alwaysOriginal)
             let im1b = UIImage(systemName: "bubble.middle.bottom.fill", withConfiguration: symbolConfig)?.withTintColor(UIColor.black.withAlphaComponent(1), renderingMode: .alwaysOriginal)
             
@@ -42,38 +43,33 @@ class ViewController: UITabBarController, UITabBarControllerDelegate {
             
             // Create Tab one
             self.tabOne = UINavigationController(rootViewController: self.firstView)
-            let tabOneBarItem = UITabBarItem(title: "", image: im1, selectedImage: im1b)
-            self.tabOne.tabBarItem = tabOneBarItem
+            self.tabOne.tabBarItem = UITabBarItem(title: "", image: im1, selectedImage: im1b)
             self.tabOne.navigationBar.backgroundColor = UIColor.white
             self.tabOne.navigationBar.barTintColor = UIColor.white
             self.tabOne.tabBarItem.tag = 1
             
             // Create Tab two
             self.tabTwo = UINavigationController(rootViewController: self.secondView)
-            let tabTwoBarItem = UITabBarItem(title: "", image: im2, selectedImage: im2b)
-            self.tabTwo.tabBarItem = tabTwoBarItem
+            self.tabTwo.tabBarItem = UITabBarItem(title: "", image: im2, selectedImage: im2b)
             self.tabTwo.navigationBar.backgroundColor = UIColor.white
             self.tabTwo.navigationBar.barTintColor = UIColor.white
             self.tabTwo.tabBarItem.tag = 2
             
             // Create Tab three
             self.tabThree = UINavigationController(rootViewController: self.thirdView)
-            let tabThreeBarItem = UITabBarItem(title: "", image: im3, selectedImage: im3b)
-            self.tabThree.tabBarItem = tabThreeBarItem
+            self.tabThree.tabBarItem = UITabBarItem(title: "", image: im3, selectedImage: im3b)
             self.tabThree.navigationBar.backgroundColor = UIColor.white
             self.tabThree.navigationBar.barTintColor = UIColor.white
             self.tabThree.tabBarItem.tag = 3
             
             // Create Tab four
             self.tabFour = UINavigationController(rootViewController: self.fourthView)
-            let tabFourBarItem = UITabBarItem(title: "", image: im4, selectedImage: im4b)
-            self.tabFour.tabBarItem = tabFourBarItem
+            self.tabFour.tabBarItem = UITabBarItem(title: "", image: im4, selectedImage: im4b)
             self.tabFour.navigationBar.backgroundColor = UIColor.white
             self.tabFour.navigationBar.barTintColor = UIColor.white
             self.tabFour.tabBarItem.tag = 4
             
-            let viewControllerList = [self.tabOne, self.tabTwo, self.tabThree, self.tabFour]
-            self.viewControllers = viewControllerList
+            self.viewControllers = [self.tabOne, self.tabTwo, self.tabThree, self.tabFour]
         }
     }
     
