eb51bea5d21b773f4867a8a1a0b0b3adca6d657e
Shihab Mehboob <shihab@IHQs-MacBook-Pro.local>
2019-09-11 15:38:12 +0100

    Initial commit

---
+++ Mast/Mast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mast
+//
+//  Created by Shihab Mehboob on 11/09/2019.
+//  Copyright © 2019 Shihab Mehboob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
