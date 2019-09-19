0e8a1bbf457ce2e65abf1e8a0d3e662a600a3a6d
Shihab Mehboob <shihab@IHQs-MacBook-Pro.local>
2019-09-19 14:10:21 +0100

    Initial profile

--- Mast/Mast/Cells/ProfileCell.swift
+++ Mast/Mast/Cells/ProfileCell.swift
@@ -16,6 +16,8 @@ class ProfileCell: UITableViewCell {
     var username = UILabel()
     var usertag = UILabel()
     var content = UILabel()
+    var followers = UIButton()
+    var joined = UILabel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -63,6 +65,24 @@ class ProfileCell: UITableViewCell {
         content.numberOfLines = 0
         contentView.addSubview(content)
         
+        followers.translatesAutoresizingMaskIntoConstraints = false
+        followers.setTitleColor(GlobalStruct.baseTint, for: .normal)
+        followers.contentHorizontalAlignment = .leading
+        followers.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        followers.titleLabel?.adjustsFontForContentSizeCategory = true
+        followers.titleLabel?.numberOfLines = 1
+        followers.titleLabel?.lineBreakMode = .byTruncatingTail
+        contentView.addSubview(followers)
+        
+        joined.translatesAutoresizingMaskIntoConstraints = false
+        joined.textColor = UIColor(named: "baseBlack")!.withAlphaComponent(0.85)
+        joined.textAlignment = .natural
+        joined.font = UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .caption1).pointSize)
+        joined.isUserInteractionEnabled = false
+        joined.adjustsFontForContentSizeCategory = true
+        joined.numberOfLines = 0
+        contentView.addSubview(joined)
+        
         username.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
         usertag.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         
@@ -72,6 +92,8 @@ class ProfileCell: UITableViewCell {
             "username" : username,
             "usertag" : usertag,
             "content" : content,
+            "followers" : followers,
+            "joined" : joined,
         ]
         
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[header]-0-|", options: [], metrics: nil, views: viewsDict))
@@ -79,9 +101,11 @@ class ProfileCell: UITableViewCell {
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[username]-20-|", options: [], metrics: nil, views: viewsDict))
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[usertag]-20-|", options: [], metrics: nil, views: viewsDict))
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[content]-20-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[followers]-20-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[joined]-20-|", options: [], metrics: nil, views: viewsDict))
         
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[header(140)]-(>=40)-|", options: [], metrics: nil, views: viewsDict))
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-100-[profile(80)]-10-[username]-2-[usertag]-5-[content]-(>=15)-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-100-[profile(80)]-12-[username]-2-[usertag]-8-[content]-8-[followers]-8-[joined]-(>=15)-|", options: [], metrics: nil, views: viewsDict))
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -90,8 +114,39 @@ class ProfileCell: UITableViewCell {
     
     func configure(_ acc: Account) {
         self.username.text = acc.displayName
-        self.usertag.text = "@\(acc.username)"
+        self.usertag.text = "@\(acc.acct)"
         self.content.text = acc.note.stripHTML()
+        self.joined.text = acc.createdAt.toString(dateStyle: .medium, timeStyle: .medium)
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        let formattedNumber = numberFormatter.string(from: NSNumber(value: acc.followersCount))
+        let numberFormatter2 = NumberFormatter()
+        numberFormatter2.numberStyle = NumberFormatter.Style.decimal
+        let formattedNumber2 = numberFormatter2.string(from: NSNumber(value: acc.followingCount))
+        
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: UIFont.preferredFont(forTextStyle: .body).pointSize - 4, weight: .bold)
+        let normalFont = UIFont.boldSystemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize - 2)
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(systemName: "person.and.person", withConfiguration: symbolConfig)?.withTintColor(UIColor(named: "baseBlack")!.withAlphaComponent(0.35), renderingMode: .alwaysOriginal)
+        let attachment2 = NSTextAttachment()
+        attachment2.image = UIImage(systemName: "arrow.upright.circle", withConfiguration: symbolConfig)?.withTintColor(UIColor(named: "baseBlack")!.withAlphaComponent(0.35), renderingMode: .alwaysOriginal)
+        let attStringNewLine = NSMutableAttributedString(string: "\(formattedNumber ?? "0")", attributes: [NSAttributedString.Key.font : normalFont, NSAttributedString.Key.foregroundColor : UIColor(named: "baseBlack")!.withAlphaComponent(1)])
+        let attStringNewLine2 = NSMutableAttributedString(string: "\(formattedNumber2 ?? "0")", attributes: [NSAttributedString.Key.font : normalFont, NSAttributedString.Key.foregroundColor : UIColor(named: "baseBlack")!.withAlphaComponent(1)])
+        let attString = NSAttributedString(attachment: attachment)
+        let attString2 = NSAttributedString(attachment: attachment2)
+        let fullString = NSMutableAttributedString(string: "")
+        let spaceString0 = NSMutableAttributedString(string: " ")
+        let spaceString = NSMutableAttributedString(string: "  ")
+        fullString.append(attString)
+        fullString.append(spaceString0)
+        fullString.append(attStringNewLine)
+        fullString.append(spaceString)
+        fullString.append(attString2)
+        fullString.append(spaceString0)
+        fullString.append(attStringNewLine2)
+        self.followers.setAttributedTitle(fullString, for: .normal)
+        
         guard let imageURL = URL(string: acc.avatar) else { return }
         DispatchQueue.global().async {
             guard let imageData = try? Data(contentsOf: imageURL) else { return }
