1220a08501b62da019d06a883dd71ad6f10acf43
Shihab Mehboob <shihab@IHQs-MacBook-Pro.local>
2019-09-19 13:57:01 +0100

    Initial profile

---
+++ Mast/Mast/Cells/ProfileCell.swift
@@ -0,0 +1,115 @@
+//
+//  ProfileCell.swift
+//  Mast
+//
+//  Created by Shihab Mehboob on 19/09/2019.
+//  Copyright © 2019 Shihab Mehboob. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileCell: UITableViewCell {
+    
+    var header = UIImageView()
+    var profile = UIImageView()
+    var username = UILabel()
+    var usertag = UILabel()
+    var content = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.backgroundColor = UIColor(named: "baseWhite")
+        header.isUserInteractionEnabled = true
+        header.contentMode = .scaleAspectFill
+        contentView.addSubview(header)
+        
+        profile.translatesAutoresizingMaskIntoConstraints = false
+        profile.layer.cornerRadius = 40
+        profile.backgroundColor = UIColor(named: "baseWhite")
+        profile.isUserInteractionEnabled = true
+        contentView.addSubview(profile)
+        
+        username.translatesAutoresizingMaskIntoConstraints = false
+        username.textColor = UIColor(named: "baseBlack")
+        username.textAlignment = .natural
+        username.font = UIFont.boldSystemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        username.isUserInteractionEnabled = false
+        username.adjustsFontForContentSizeCategory = true
+        username.numberOfLines = 1
+        username.lineBreakMode = .byTruncatingTail
+        contentView.addSubview(username)
+        
+        usertag.translatesAutoresizingMaskIntoConstraints = false
+        usertag.textColor = UIColor(named: "baseBlack")!.withAlphaComponent(0.45)
+        usertag.textAlignment = .natural
+        usertag.font = UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        usertag.isUserInteractionEnabled = false
+        usertag.adjustsFontForContentSizeCategory = true
+        usertag.numberOfLines = 1
+        usertag.lineBreakMode = .byTruncatingTail
+        contentView.addSubview(usertag)
+        
+        content.translatesAutoresizingMaskIntoConstraints = false
+        content.textColor = UIColor(named: "baseBlack")!.withAlphaComponent(0.85)
+        content.textAlignment = .natural
+        content.font = UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        content.isUserInteractionEnabled = false
+        content.adjustsFontForContentSizeCategory = true
+        content.numberOfLines = 0
+        contentView.addSubview(content)
+        
+        username.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        usertag.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        let viewsDict = [
+            "header" : header,
+            "profile" : profile,
+            "username" : username,
+            "usertag" : usertag,
+            "content" : content,
+        ]
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[header]-0-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[profile(80)]", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[username]-20-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[usertag]-20-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[content]-20-|", options: [], metrics: nil, views: viewsDict))
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[header(100)]-(>=40)-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-60-[profile(80)]-10-[username]-3-[usertag]-3-[content]-(>=15)-|", options: [], metrics: nil, views: viewsDict))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(_ acc: Account) {
+        self.username.text = acc.displayName
+        self.usertag.text = acc.username
+        self.content.text = acc.note.stripHTML()
+        guard let imageURL = URL(string: acc.avatar) else { return }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }
+            let image = UIImage(data: imageData)
+            DispatchQueue.main.async {
+                self.profile.image = image
+                self.profile.layer.masksToBounds = true
+            }
+        }
+        guard let imageURL2 = URL(string: acc.header) else { return }
+        DispatchQueue.global().async {
+            guard let imageData2 = try? Data(contentsOf: imageURL2) else { return }
+            let image = UIImage(data: imageData2)
+            DispatchQueue.main.async {
+                self.header.image = image
+                self.header.layer.masksToBounds = true
+            }
+        }
+    }
+}
+
+
+
--- Mast/Mast/Views/FifthViewController.swift
+++ Mast/Mast/Views/FifthViewController.swift
@@ -9,10 +9,19 @@
 import Foundation
 import UIKit
 
-class FifthViewController: UIViewController {
+class FifthViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var tableView = UITableView()
     var isYou = true
     var pickedCurrentUser: Account!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Table
+        let tableHeight = (UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0) + (self.navigationController?.navigationBar.bounds.height ?? 0)
+        self.tableView.frame = CGRect(x: 0, y: tableHeight, width: self.view.bounds.width, height: (self.view.bounds.height) - tableHeight)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +46,21 @@ class FifthViewController: UIViewController {
         if self.isYou {
             self.navigationItem.setLeftBarButton(settingsButton, animated: true)
         }
+        
+        // Table
+        self.tableView.register(ProfileCell.self, forCellReuseIdentifier: "ProfileCell")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.separatorStyle = .singleLine
+        self.tableView.separatorColor = UIColor(named: "baseBlack")?.withAlphaComponent(0.24)
+        self.tableView.backgroundColor = UIColor.clear
+        self.tableView.layer.masksToBounds = true
+        self.tableView.estimatedRowHeight = UITableView.automaticDimension
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.showsVerticalScrollIndicator = true
+        self.tableView.tableFooterView = UIView()
+        self.view.addSubview(self.tableView)
+        self.tableView.reloadData()
     }
     
     @objc func settingsTapped() {
@@ -45,6 +69,33 @@ class FifthViewController: UIViewController {
     
     @objc func addTapped() {
         NotificationCenter.default.post(name: Notification.Name(rawValue: "addTapped"), object: self)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as! ProfileCell
+        if self.isYou {
+            cell.configure(GlobalStruct.currentUser)
+        } else {
+            cell.configure(self.pickedCurrentUser)
+        }
+        cell.backgroundColor = UIColor(named: "baseWhite")
+        let bgColorView = UIView()
+        bgColorView.backgroundColor = UIColor.clear
+        cell.selectedBackgroundView = bgColorView
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     func removeTabbarItemsText() {
