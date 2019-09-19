9ed6e268b697a5606e7a7d2c69ae772c7957011f
Shihab Mehboob <shihab@IHQs-MacBook-Pro.local>
2019-09-19 13:07:02 +0100

    Pull to refresh

--- Mast/Mast/Views/SecondViewController.swift
+++ Mast/Mast/Views/SecondViewController.swift
@@ -14,6 +14,8 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     var tableView = UITableView()
     var tableView2 = UITableView()
     let segment: UISegmentedControl = UISegmentedControl(items: ["Activity".localized, "Metrics".localized])
+    var refreshControl = UIRefreshControl()
+    let top1 = UIButton()
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -67,6 +69,9 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
         self.view.addSubview(self.tableView)
         self.tableView.reloadData()
         
+        self.refreshControl.addTarget(self, action: #selector(refresh(_:)), for: UIControl.Event.valueChanged)
+        self.tableView.addSubview(self.refreshControl)
+        
         self.tableView2.register(DirectCell.self, forCellReuseIdentifier: "DirectCell")
         self.tableView2.delegate = self
         self.tableView2.dataSource = self
@@ -81,6 +86,70 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
         self.tableView2.alpha = 0
         self.view.addSubview(self.tableView2)
         self.tableView2.reloadData()
+        
+        // Top buttons
+        let startHeight = (UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0) + (self.navigationController?.navigationBar.bounds.height ?? 0) + (self.segment.bounds.height) + 10
+        let symbolConfig2 = UIImage.SymbolConfiguration(pointSize: 30, weight: .regular)
+        
+        self.top1.frame = CGRect(x: Int(self.view.bounds.width) - 48, y: Int(startHeight + 6), width: 38, height: 38)
+        self.top1.setImage(UIImage(systemName: "chevron.up.circle.fill", withConfiguration: symbolConfig2)?.withTintColor(GlobalStruct.baseTint, renderingMode: .alwaysOriginal), for: .normal)
+        self.top1.backgroundColor = UIColor(named: "baseWhite")
+        self.top1.layer.cornerRadius = 19
+        self.top1.alpha = 0
+        self.top1.addTarget(self, action: #selector(self.didTouchTop1), for: .touchUpInside)
+        self.view.addSubview(self.top1)
+    }
+    
+    @objc func didTouchTop1() {
+        self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+        UIView.animate(withDuration: 0.18, delay: 0, options: .curveEaseOut, animations: {
+            self.top1.alpha = 0
+            self.top1.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        }) { (completed: Bool) in
+        }
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        UIView.animate(withDuration: 0.18, delay: 0, options: .curveEaseOut, animations: {
+            self.top1.alpha = 0
+            self.top1.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        }) { (completed: Bool) in
+        }
+    }
+    
+    @objc func refresh(_ sender: AnyObject) {
+        self.refreshControl.endRefreshing()
+        let request = Notifications.all(range: .since(id: GlobalStruct.notifications.first?.id ?? "", limit: nil))
+        GlobalStruct.client.run(request) { (statuses) in
+            if let stat = (statuses.value) {
+                if stat.isEmpty {} else {
+                    DispatchQueue.main.async {
+                        self.top1.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+                        UIView.animate(withDuration: 0.18, delay: 0, options: .curveEaseOut, animations: {
+                            self.top1.alpha = 1
+                            self.top1.transform = CGAffineTransform(scaleX: 1, y: 1)
+                        }) { (completed: Bool) in
+                        }
+                        let indexPaths = (0..<stat.count).map {
+                            IndexPath(row: $0, section: 0)
+                        }
+                        GlobalStruct.notifications = stat + GlobalStruct.notifications
+                        self.tableView.beginUpdates()
+                        UIView.setAnimationsEnabled(false)
+                        var heights: CGFloat = 0
+                        let _ = indexPaths.map {
+                            if let cell = self.tableView.cellForRow(at: $0) as? TootCell {
+                                heights += cell.bounds.height
+                            }
+                        }
+                        self.tableView.insertRows(at: indexPaths, with: UITableView.RowAnimation.none)
+                        self.tableView.setContentOffset(CGPoint(x: 0, y: heights), animated: false)
+                        self.tableView.endUpdates()
+                        UIView.setAnimationsEnabled(true)
+                    }
+                }
+            }
+        }
     }
     
     @objc func changeSegment(_ segment: UISegmentedControl) {
@@ -93,6 +162,11 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
             self.tableView.alpha = 0
             self.tableView2.alpha = 1
             self.tableView2.reloadData()
+        }
+        UIView.animate(withDuration: 0.18, delay: 0, options: .curveEaseOut, animations: {
+            self.top1.alpha = 0
+            self.top1.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        }) { (completed: Bool) in
         }
     }
     
--- Mast/Mast/Views/ThirdViewController.swift
+++ Mast/Mast/Views/ThirdViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 class ThirdViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var tableView = UITableView()
+    var refreshControl = UIRefreshControl()
+    let top1 = UIButton()
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -57,6 +59,73 @@ class ThirdViewController: UIViewController, UITableViewDataSource, UITableViewD
         self.tableView.tableFooterView = UIView()
         self.view.addSubview(self.tableView)
         self.tableView.reloadData()
+        
+        self.refreshControl.addTarget(self, action: #selector(refresh(_:)), for: UIControl.Event.valueChanged)
+        self.tableView.addSubview(self.refreshControl)
+        
+        // Top buttons
+        let startHeight = (UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0) + (self.navigationController?.navigationBar.bounds.height ?? 0) + (self.segment.bounds.height) + 10
+        let symbolConfig2 = UIImage.SymbolConfiguration(pointSize: 30, weight: .regular)
+        
+        self.top1.frame = CGRect(x: Int(self.view.bounds.width) - 48, y: Int(startHeight + 6), width: 38, height: 38)
+        self.top1.setImage(UIImage(systemName: "chevron.up.circle.fill", withConfiguration: symbolConfig2)?.withTintColor(GlobalStruct.baseTint, renderingMode: .alwaysOriginal), for: .normal)
+        self.top1.backgroundColor = UIColor(named: "baseWhite")
+        self.top1.layer.cornerRadius = 19
+        self.top1.alpha = 0
+        self.top1.addTarget(self, action: #selector(self.didTouchTop1), for: .touchUpInside)
+        self.view.addSubview(self.top1)
+    }
+    
+    @objc func didTouchTop1() {
+        self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+        UIView.animate(withDuration: 0.18, delay: 0, options: .curveEaseOut, animations: {
+            self.top1.alpha = 0
+            self.top1.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        }) { (completed: Bool) in
+        }
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        UIView.animate(withDuration: 0.18, delay: 0, options: .curveEaseOut, animations: {
+            self.top1.alpha = 0
+            self.top1.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        }) { (completed: Bool) in
+        }
+    }
+    
+    @objc func refresh(_ sender: AnyObject) {
+        self.refreshControl.endRefreshing()
+        let request = Timelines.conversations(range: .since(id: GlobalStruct.notificationsDirect.first?.id ?? "", limit: nil))
+        GlobalStruct.client.run(request) { (statuses) in
+            if let stat = (statuses.value) {
+                if stat.isEmpty {} else {
+                    DispatchQueue.main.async {
+                        self.top1.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+                        UIView.animate(withDuration: 0.18, delay: 0, options: .curveEaseOut, animations: {
+                            self.top1.alpha = 1
+                            self.top1.transform = CGAffineTransform(scaleX: 1, y: 1)
+                        }) { (completed: Bool) in
+                        }
+                        let indexPaths = (0..<stat.count).map {
+                            IndexPath(row: $0, section: 0)
+                        }
+                        GlobalStruct.notificationsDirect = stat + GlobalStruct.notificationsDirect
+                        self.tableView.beginUpdates()
+                        UIView.setAnimationsEnabled(false)
+                        var heights: CGFloat = 0
+                        let _ = indexPaths.map {
+                            if let cell = self.tableView.cellForRow(at: $0) as? TootCell {
+                                heights += cell.bounds.height
+                            }
+                        }
+                        self.tableView.insertRows(at: indexPaths, with: UITableView.RowAnimation.none)
+                        self.tableView.setContentOffset(CGPoint(x: 0, y: heights), animated: false)
+                        self.tableView.endUpdates()
+                        UIView.setAnimationsEnabled(true)
+                    }
+                }
+            }
+        }
     }
     
     @objc func settingsTapped() {
