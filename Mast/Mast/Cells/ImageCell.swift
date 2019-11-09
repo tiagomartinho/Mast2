902b72b4133fddea4cdba09365805a9ba80da496
Shihab <shihab.mehboob@hotmail.com>
2019-11-09 15:57:47 +0000

    Minor changes

---
+++ Mast/Mast/Cells/ImageCell.swift
@@ -0,0 +1,66 @@
+//
+//  ImageCell.swift
+//  Mast
+//
+//  Created by Shihab Mehboob on 09/11/2019.
+//  Copyright © 2019 Shihab Mehboob. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    var bgImage = UIImageView()
+    var image = UIImageView()
+    var imageCountTag = UIButton()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    public func configure() {
+        self.bgImage.backgroundColor = UIColor.clear
+        self.bgImage.frame = CGRect(x: 0, y: 0, width: contentView.frame.size.width, height: contentView.frame.size.height)
+        self.bgImage.layer.cornerRadius = 20
+        self.bgImage.layer.cornerCurve = .continuous
+        contentView.addSubview(bgImage)
+        
+        self.image.backgroundColor = UIColor.clear
+        self.image.frame = CGRect(x: 0, y: 0, width: contentView.frame.size.width, height: contentView.frame.size.height)
+        self.image.layer.cornerRadius = 20
+        self.image.layer.cornerCurve = .continuous
+        contentView.addSubview(image)
+        
+        imageCountTag.isUserInteractionEnabled = false
+        imageCountTag.setTitle("", for: .normal)
+        imageCountTag.backgroundColor = UIColor.clear
+        imageCountTag.translatesAutoresizingMaskIntoConstraints = false
+        imageCountTag.layer.cornerRadius = 7
+        imageCountTag.titleLabel?.font = UIFont.boldSystemFont(ofSize: 12)
+        imageCountTag.layer.shadowColor = UIColor.black.cgColor
+        imageCountTag.layer.shadowOffset = CGSize(width: 0, height: 7)
+        imageCountTag.layer.shadowRadius = 10
+        imageCountTag.layer.shadowOpacity = 0.22
+        imageCountTag.layer.masksToBounds = false
+        imageCountTag.alpha = 1
+        contentView.addSubview(imageCountTag)
+        
+        let viewsDict = [
+            "countTag" : imageCountTag,
+        ]
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[countTag(30)]-(>=12)-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[countTag(22)]-(>=12)-|", options: [], metrics: nil, views: viewsDict))
+    }
+}
+
+
+
+
+
+
---
+++ Mast/Mast/Views/IconSettingsViewController.swift
@@ -0,0 +1,108 @@
+//
+//  IconSettingsViewController.swift
+//  Mast
+//
+//  Created by Shihab Mehboob on 09/11/2019.
+//  Copyright © 2019 Shihab Mehboob. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IconSettingsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    var collectionView: UICollectionView!
+    var appArrayIcons = ["icon1", "icon2", "icon3", "icon4", "icon5", "icon6", "icon7", "icon8", "icon9", "icon10", "icon11", "icon12", "icon13", "icon14", "icon15", "icon16"]
+    var appArray = ["AppIcon-1", "AppIcon-2", "AppIcon-3", "AppIcon-4", "AppIcon-5", "AppIcon-6", "AppIcon-7", "AppIcon-8", "AppIcon-9", "AppIcon-10", "AppIcon-11", "AppIcon-12", "AppIcon-13", "AppIcon-14", "AppIcon-15", "AppIcon-16"]
+    
+    override func viewDidLayoutSubviews() {
+        self.collectionView.frame = CGRect(x: self.view.safeAreaInsets.left, y: 0, width: self.view.bounds.width - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: self.view.bounds.height)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(named: "lighterBaseWhite")
+        self.title = "App Icon".localized
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor(named: "baseBlack")!]
+
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            let layout = ColumnFlowLayout(
+                cellsPerRow: 7,
+                minimumInteritemSpacing: 5,
+                minimumLineSpacing: 5,
+                sectionInset: UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
+            )
+            self.collectionView = UICollectionView(frame: CGRect(x: self.view.safeAreaInsets.left, y: 0, width: self.view.bounds.width - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: self.view.bounds.height), collectionViewLayout: layout)
+        } else {
+            let layout = ColumnFlowLayout(
+                cellsPerRow: 4,
+                minimumInteritemSpacing: 5,
+                minimumLineSpacing: 5,
+                sectionInset: UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
+            )
+            self.collectionView = UICollectionView(frame: CGRect(x: self.view.safeAreaInsets.left, y: 0, width: self.view.bounds.width - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: self.view.bounds.height), collectionViewLayout: layout)
+        }
+        self.collectionView.backgroundColor = UIColor.clear
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.register(ImageCell.self, forCellWithReuseIdentifier: "ImageCell")
+        self.view.addSubview(self.collectionView)
+        self.collectionView.reloadData()
+    }
+    
+    //MARK: CollectionView
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.appArrayIcons.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            let x = 7
+            let y = (self.view.bounds.width) - 30
+            let z = CGFloat(y)/CGFloat(x)
+            return CGSize(width: z - 7.5, height: z - 7.5)
+        } else {
+            let x = 4
+            let y = (self.view.bounds.width) - 30
+            let z = CGFloat(y)/CGFloat(x)
+            return CGSize(width: z - 7.5, height: z - 7.5)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        cell.configure()
+        cell.image.image = nil
+        cell.image.image = UIImage(named: self.appArrayIcons[indexPath.row])
+        cell.image.contentMode = .scaleAspectFill
+        cell.layer.cornerRadius = 20
+        cell.layer.cornerCurve = .continuous
+        cell.image.layer.cornerRadius = 20
+        cell.image.layer.cornerCurve = .continuous
+        cell.image.layer.masksToBounds = true
+        cell.imageCountTag.alpha = 0
+        cell.image.frame.size.width = cell.frame.size.width
+        cell.image.frame.size.height = cell.frame.size.height
+        cell.backgroundColor = UIColor.clear
+        cell.backgroundColor = UIColor(named: "baseGray")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let imp = UIImpactFeedbackGenerator(style: .light)
+        imp.impactOccurred()
+        if indexPath.item == 0 {
+            UIApplication.shared.setAlternateIconName(nil)
+        } else {
+            UIApplication.shared.setAlternateIconName(self.appArray[indexPath.row])
+        }
+    }
+}
+
+
+
+
+
+
