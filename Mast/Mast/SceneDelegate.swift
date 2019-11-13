45a747aa39ddda0522e258db6493b23594ad0d45
Shihab <shihab.mehboob@hotmail.com>
2019-11-13 19:44:32 +0000

    Minor changes

--- Mast/Mast/SceneDelegate.swift
+++ Mast/Mast/SceneDelegate.swift
@@ -120,6 +120,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
         
+        UIApplication.shared.applicationIconBadgeNumber = 0
         self.window?.tintColor = UIColor(named: "baseBlack")!
         NotificationCenter.default.post(name: Notification.Name(rawValue: "startHaptics"), object: self)
         
