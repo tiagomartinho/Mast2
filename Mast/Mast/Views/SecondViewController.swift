0d829481c37126ad6920af39d084451c837cd025
Shihab <shihab.mehboob@hotmail.com>
2019-11-23 10:08:04 +0000

    Minor changes

--- Mast/Mast/Views/SecondViewController.swift
+++ Mast/Mast/Views/SecondViewController.swift
@@ -219,6 +219,24 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
         self.top1.addTarget(self, action: #selector(self.didTouchTop1), for: .touchUpInside)
         self.top1.accessibilityLabel = "Top".localized
         self.view.addSubview(self.top1)
+        
+        if GlobalStruct.notifications.isEmpty {
+            let request4 = Notifications.all(range: .default, typesToExclude: self.notTypes)
+            GlobalStruct.client.run(request4) { (statuses) in
+                if let stat = (statuses.value) {
+                    DispatchQueue.main.async {
+                        GlobalStruct.notifications = stat
+                        #if targetEnvironment(macCatalyst)
+                        NotificationCenter.default.post(name: Notification.Name(rawValue: "refreshTable"), object: nil)
+                        #elseif !targetEnvironment(macCatalyst)
+                        if UIDevice.current.userInterfaceIdiom == .pad && self.isSplitOrSlideOver == false {
+                            NotificationCenter.default.post(name: Notification.Name(rawValue: "refreshTable"), object: nil)
+                        }
+                        #endif
+                    }
+                }
+            }
+        }
     }
     
     @objc func didTouchTop1() {
