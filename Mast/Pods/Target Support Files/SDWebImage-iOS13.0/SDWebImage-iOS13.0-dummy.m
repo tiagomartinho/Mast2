79de1de8b1645a064cb4a8c9d93e11189439699f
Shihab <shihab.mehboob@hotmail.com>
2019-11-23 13:33:35 +0000

    Minor changes

--- Mast/Pods/Target Support Files/SDWebImage-iOS13.0/SDWebImage-iOS13.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SDWebImage_iOS13_0 : NSObject
-@end
-@implementation PodsDummy_SDWebImage_iOS13_0
-@end
--- Mast/Pods/Target Support Files/SDWebImage-iOS13.1/SDWebImage-iOS13.1-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SDWebImage_iOS13_1 : NSObject
-@end
-@implementation PodsDummy_SDWebImage_iOS13_1
-@end
