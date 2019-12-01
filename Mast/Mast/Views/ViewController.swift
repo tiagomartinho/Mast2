47c96fce049a4987dc6b3c0fc12f857de48abee5
Shihab <shihab.mehboob@hotmail.com>
2019-12-01 19:29:03 +0000

    Minor changes

--- Mast/Mast/Views/ViewController.swift
+++ Mast/Mast/Views/ViewController.swift
@@ -581,3 +581,17 @@ extension UITextView {
     }
     #endif
 }
+
+extension UIImage {
+    func resizeImageWith(newSize: CGSize) -> UIImage {
+        let horizontalRatio = newSize.width / size.width
+        let verticalRatio = newSize.height / size.height
+        let ratio = min(horizontalRatio, verticalRatio)
+        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        draw(in: CGRect(origin: CGPoint(x: 0, y: 0), size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
